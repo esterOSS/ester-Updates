6fd090158cc21616e53bc50e589b4e14392dfe90
matik <89382946+matu6968@users.noreply.github.com>
2023-11-20 17:51:49 +0100

    Update update.sh

--- latest/update.sh
+++ latest/update.sh
@@ -4,7 +4,7 @@ cd temp
 sudo wget https://github.com/esterOSS/assets/releases/download/1.0.0/esterOSGUIupdater
 sudo mv esterOSGUIupdater esteros-updater
 sudo killall -9 esteros-updater
-sudo cp esteros-updater /home/ximi-pc/assets/esteros-updater/esteros-updater/src/
+sudo cp esteros-updater /bin
 sudo rm esteros-updater
 cd ..
 sudo rmdir --ignore-fail-on-non-empty temp
