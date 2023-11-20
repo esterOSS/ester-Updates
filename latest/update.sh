59b9dcb76a7bd6e00e1eb5d93c139e99fce49d51
matik <89382946+matu6968@users.noreply.github.com>
2023-11-20 17:19:29 +0100

    Update update.sh

--- latest/update.sh
+++ latest/update.sh
@@ -2,5 +2,5 @@
 mkdir temp
 cd temp
 sudo wget https://github.com/esterOSS/assets/releases/download/1.0.0/esterOSGUIupdater
-rm esterOSGUIupdater esteros-updater
+mv esterOSGUIupdater esteros-updater
 sudo cp esteros-updater /home/ximi-pc/assets/esteros-updater/esteros-updater/src/
