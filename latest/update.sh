ecf4481ddec84cd75ef577aa86bcb95c9fe0f320
matik <89382946+matu6968@users.noreply.github.com>
2023-11-20 20:57:51 +0100

    fix root access

--- latest/update.sh
+++ latest/update.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-sudo mkdir temp
+pkexec mkdir temp
 cd temp
-sudo wget https://github.com/esterOSS/assets/releases/download/1.0.0/esterOSGUIupdater
-sudo mv esterOSGUIupdater esteros-updater
-sudo killall -9 esteros-updater
-sudo cp esteros-updater /bin
-sudo rm esteros-updater
+pkexec wget https://github.com/esterOSS/assets/releases/download/1.0.0/esterOSGUIupdater
+pkexec mv esterOSGUIupdater esteros-updater
+pkexec killall -9 esteros-updater
+pkexec cp esteros-updater /bin
+pkexec rm esteros-updater
 cd ..
-sudo rmdir --ignore-fail-on-non-empty temp
+pkexec rmdir --ignore-fail-on-non-empty temp
 echo Updating updater done!
 ./esteros-updater
